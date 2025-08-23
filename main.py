0834e9aa040cbbdad303087d36ccf0bd71c5d6b2
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-08-23 14:49:12 -0400

    Updated main.py and characters and added gitignore

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import tkinter as tk
-import random
+from tkinter import *
+from tkinter import ttk
 import json
+import random
+from characters import ASCII, UTF8
 
+def printStr(length, charSet):
+    for i in range(length):
+        char = random.choice(charSet)
+        print(char)
+
+print(printStr(10, UTF8))
+
+'''
+root = Tk()
+root.title("Random String Generator")
+
+mainframe =  ttk.Frame(root, )
+'''
