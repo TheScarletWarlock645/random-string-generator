20b311caeb94000b2c051e1276ae5e31b38ce2f5
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-08-23 17:46:21 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,19 +1,66 @@
 from tkinter import *
-from tkinter import ttk
-import json
+from tkinter import filedialog
 import random
 from characters import ASCII, UTF8
 
-def printStr(length, charSet):
+def genStr(length, charSet):
+    result = ""
     for i in range(length):
         char = random.choice(charSet)
-        print(char)
+        result += char
+    return result
 
-print(printStr(10, UTF8))
+def printStr(length, charSet):
+    global generatedString
+    generatedString = genStr(length, charSet)
+    string = Label(root, bg="white", fg="black", text=generatedString)
+    string.pack()
 
-'''
+def sel():
+    global charSet
+    if selected_option.get() == "utf8":
+        charSet = UTF8
+    elif selected_option.get() == "ascii":
+        charSet = ASCII
+
+def save():
+    if generatedString:
+        fileDialog = filedialog.asksaveasfilename(
+            defaultextension=".txt"
+        )
+
+        if fileDialog:
+            with open(fileDialog, 'w') as f:
+                f.write(generatedString)
+
 root = Tk()
 root.title("Random String Generator")
+root.geometry("300x275")
 
-mainframe =  ttk.Frame(root, )
-'''
+selected_option = StringVar(value="utf8")
+charSet = UTF8
+
+lengthLabel = Label(root, text="Enter a number:")
+lengthLabel.pack(padx=5, pady=5)
+
+lengthInput = Entry(root, width=3)
+lengthInput.insert(0, "10")
+lengthInput.pack(padx=5, pady=5)
+
+charLabel = Label(root, text="Choose a character set:")
+charLabel.pack(pady=(20, 5))
+
+rbtnUTF8 = Radiobutton(root, text="UTF-8", variable=selected_option, value="utf8", command=sel)
+rbtnUTF8.pack(pady=2)
+rbtnASCII = Radiobutton(root, text="ASCII", variable=selected_option, value="ascii", command=sel)
+rbtnASCII.pack(pady=2)
+
+frameBtn = Frame(root)
+frameBtn.pack(pady=20)
+
+btnGenerate = Button(frameBtn, text="Generate string", command= lambda: printStr(int(lengthInput.get()), charSet))
+btnGenerate.pack(side="left", padx=5)
+btnSave = Button(frameBtn, text="Save to file", command=save)
+btnSave.pack(side="left", padx=5)
+
+root.mainloop()
