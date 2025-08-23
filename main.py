970cdbf360a7d158fbbc27245f73f9b451200c86
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-08-23 18:01:54 -0400

    Updated for improved selection of characters

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 from tkinter import *
 from tkinter import filedialog
 import random
-from characters import ASCII, UTF8
+import string
 
-def genStr(length, charSet):
-    result = ""
-    for i in range(length):
-        char = random.choice(charSet)
-        result += char
-    return result
+def genStr(length, charset_type):
+    if charset_type == "ascii":
+        chars = string.printable.rstrip('\t\n\r\x0b\x0c')
+        return ''.join(random.choices(chars, k=length))
+    else:
+        utf8_ranges = [
+            (0x0020, 0x007E),   # Basic Latin
+            (0x00A0, 0x00FF),   # Latin-1 Supplement
+            (0x0100, 0x017F),   # Latin Extended-A
+            (0x2000, 0x206F),   # General Punctuation
+            (0x20A0, 0x20CF),   # Currency Symbols
+        ]
+        
+        result = []
+        for _ in range(length):
+            start, end = random.choice(utf8_ranges)
+            while True:
+                code_point = random.randint(start, end)
+                if not (0xD800 <= code_point <= 0xDFFF):
+                    try:
+                        char = chr(code_point)
+                        result.append(char)
+                        break
+                    except ValueError:
+                        continue
+        return ''.join(result)
 
-def printStr(length, charSet):
+def printStr(length, charset_type):
     global generatedString
-    generatedString = genStr(length, charSet)
+    generatedString = genStr(length, charset_type)
     stringOutput = Label(root, bg="white", fg="black", text=generatedString)
     stringOutput.pack()
-
-def sel():
-    global charSet
-    if selected_option.get() == "utf8":
-        charSet = UTF8
-    elif selected_option.get() == "ascii":
-        charSet = ASCII
 
 def save():
     if generatedString:
         fileDialog = filedialog.asksaveasfilename(
             defaultextension=".txt"
         )
-
         if fileDialog:
-            with open(fileDialog, 'w') as f:
+            with open(fileDialog, 'w', encoding='utf-8') as f:
                 f.write(generatedString)
 
 root = Tk()
@@ -38,7 +50,6 @@ root.title("Random String Generator")
 root.geometry("300x275")
 
 selected_option = StringVar(value="utf8")
-charSet = UTF8
 
 lengthLabel = Label(root, text="Enter a number:")
 lengthLabel.pack(padx=5, pady=5)
@@ -50,15 +61,15 @@ lengthInput.pack(padx=5, pady=5)
 charLabel = Label(root, text="Choose a character set:")
 charLabel.pack(pady=(20, 5))
 
-rbtnUTF8 = Radiobutton(root, text="UTF-8", variable=selected_option, value="utf8", command=sel)
+rbtnUTF8 = Radiobutton(root, text="UTF-8", variable=selected_option, value="utf8")
 rbtnUTF8.pack(pady=2)
-rbtnASCII = Radiobutton(root, text="ASCII", variable=selected_option, value="ascii", command=sel)
+rbtnASCII = Radiobutton(root, text="ASCII", variable=selected_option, value="ascii")
 rbtnASCII.pack(pady=2)
 
 frameBtn = Frame(root)
 frameBtn.pack(pady=20)
 
-btnGenerate = Button(frameBtn, text="Generate string", command= lambda: printStr(int(lengthInput.get()), charSet))
+btnGenerate = Button(frameBtn, text="Generate string", command=lambda: printStr(int(lengthInput.get()), selected_option.get()))
 btnGenerate.pack(side="left", padx=5)
 btnSave = Button(frameBtn, text="Save to file", command=save)
 btnSave.pack(side="left", padx=5)
